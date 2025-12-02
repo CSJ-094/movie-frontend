525bd4dc1fc72d9eafe4c01a330ecd257e5844ac
CSJ-094 <niznia7852@gmail.com>
2025-12-02 11:08:19 +0900

    메인

---
+++ src/pages/MovieDetailPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieDetailPage: React.FC = () => {
+  return (
+    <div>
+      <h1>영화 상세 정보</h1>
+      <p>이곳에 영화의 상세 정보가 표시됩니다.</p>
+    </div>
+  );
+};
+
+export default MovieDetailPage;
